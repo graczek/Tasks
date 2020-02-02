ac4698de3b1b5c6a54907ec4d49cea499d3c1afa
graczek <raczek.grz@gmail.com>
2020-02-02 11:15:46 +0100

    22.3 Spring Web - REST Api 2: extending client in progress

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -19,13 +19,28 @@ public class TrelloController {
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
     public void getTrelloBoards() {
 
+//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//
+//        trelloBoards.stream()
+//                .filter(b -> b.getName()!=null)
+//                .filter(b -> b.getName().contains("Kodilla"))
+//                .filter(b -> b.getId()!=null)
+//                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+
+
+        // GET request
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.stream()
-                .filter(b -> b.getName()!=null)
-                .filter(b -> b.getName().contains("Kodilla"))
-                .filter(b -> b.getId()!=null)
-                .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+        trelloBoards.forEach(trelloBoardDto -> {
+
+            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+
+            System.out.println("This board contains lists: ");
+
+            trelloBoardDto.getLists().forEach(trelloList ->
+                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+
+        });
 
     }
 }
